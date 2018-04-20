ab7f6543706162d373227e7645c8144c4a998aa9
Steven <stevenwright1012@gmail.com>
2018-04-20 09:44:28 -0600

    moved app.listen into massive

--- budget-app/server/controller.js
+++ budget-app/server/controller.js
@@ -1,9 +1,10 @@
 module.exports = {
     butt: (req, res) => {
         const db = req.app.get('db')
-
-        db.get_butts().then(res => {
-            res.status(200).send(res)
+        console.log('ive been hit');
+        
+        db.get_butts().then(result => {
+            res.status(200).send(result)
         })
     }
 }
--- budget-app/server/index.js
+++ budget-app/server/index.js
@@ -7,10 +7,10 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 
-massive(process.env.CONNECTION_STRING).then(db => {
-    app.set('db', db)
-})
 
 app.get('/api/all', con.butt)
 
-app.listen(3005, () => console.log("listening on 3005"));
+massive(process.env.CONNECTION_STRING).then(db => {
+    app.set('db', db)
+    app.listen(3005, () => console.log("listening on 3005"));
+})
